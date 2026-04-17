c31fd71926615baea2927cd994c1ef287717bd28
Asmita Hase <asmitahase@gmail.com>
2026-04-17 12:34:37 +0530

    feat: employee CTC Breakup report

---
+++ hrms/payroll/report/employee_ctc_break_up/employee_ctc_break_up.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Employee CTC Break-up"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
